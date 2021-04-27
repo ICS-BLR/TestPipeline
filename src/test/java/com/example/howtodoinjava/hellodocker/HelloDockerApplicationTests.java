da118da3ecd9e564441a4c2cfd95e981df1e9357
Mohit Jain <mohit.jain@infinite.com>
2021-04-27 16:40:03 +0530

    Added mode test cases

--- src/test/java/com/example/howtodoinjava/hellodocker/HelloDockerApplicationTests.java
+++ src/test/java/com/example/howtodoinjava/hellodocker/HelloDockerApplicationTests.java
@@ -6,26 +6,37 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertTrue;
 
-import com.example.howtodoinjava.hellodocker.HelloDockerRestController;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HelloDockerApplicationTests {
 
 	@Test
-	public void contextLoads() {		
+	public void contextLoads() {
 		System.out.println("Inside contextLoads()");
 		HelloDockerRestController helloDockerRestController = new HelloDockerRestController();
 		helloDockerRestController.helloDocker("Test Case is executed.");
 		assertTrue("message", true);
-	  }
+	}
 
 	@Test
-    public void shouldAnswerWithTrue()
-    {
+	public void shouldAnswerWithTrue() {
 		System.out.println("Inside shouldAnswerWithTrue()");
 		HelloDockerRestController helloDockerRestController = new HelloDockerRestController();
 		helloDockerRestController.helloDocker("Test Case is executed.");
-        assertTrue( true );
-    }
+		assertTrue(true);
+	}
+
+	@Test
+	public void shouldPrintMessage() {
+		MessageUtil messageUtil = new MessageUtil("Message passed from shouldPrintMessage");
+		messageUtil.printMessage();
+		assertTrue(true);
+	}
+
+	@Test
+	public void shouldSalutationMessage() {
+		MessageUtil messageUtil = new MessageUtil("Message passed from shouldSalutationMessage");
+		messageUtil.salutationMessage();
+		assertTrue(true);
+	}
 }
